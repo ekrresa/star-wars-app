8c6d868b018fa529f78c8458b5e1759800b506e1
Ochuko Ekrresa <kelvinekrresa@gmail.com>
2020-03-23 10:23:54 +0100

    Built jumbotron component

---
+++ src/components/Jumbotron/index.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Link } from '@reach/router';
+import { FiSearch } from 'react-icons/fi';
+
+import banner from '../../assets/hero-banner.jpg';
+import logo from '../../assets/logo.png';
+import './jumbotron.css';
+
+export function Jumbotron() {
+	return (
+		<header className="header" style={{ backgroundImage: `url(${banner})` }}>
+			<section className="header-logo-row">
+				<div className="header-logo-container">
+					<Link to="/">
+						<img src={logo} alt="app logo" />
+					</Link>
+				</div>
+			</section>
+
+			<section className="hero">
+				<div className="row align-center">
+					<div className="hero-img">
+						<img src={logo} alt="title" />
+					</div>
+					<div className="hero-title">
+						<h1>directory</h1>
+					</div>
+				</div>
+				<hr className="hero-linebreak" />
+
+				<p className="hero-desc">Find your favourite Characters, Starships and Planets</p>
+
+				<form className="hero-searchform">
+					<div className="hero-formcontainer">
+						<div className="hero-searchicon">
+							<FiSearch />
+						</div>
+						<input className="hero-searchinput" placeholder="Enter a search term..." />
+					</div>
+				</form>
+			</section>
+		</header>
+	);
+}
---
+++ src/pages/Home/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import { Jumbotron } from '../../components/Jumbotron';
+
+export default function Home() {
+	return <Jumbotron />;
+}
