ec00c2140cf7bf706fb7cebc1870ccfb2d961979
Ochuko Ekrresa <kelvinekrresa@gmail.com>
2020-03-24 13:29:30 +0100

    Increased max-width of container

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -1,9 +1,7 @@
-import React from './node_modules/react';
+import React from 'react';
 
 import './container.css';
 
-function Container({ children }) {
+export function Container({ children }) {
 	return <section className="container">{children}</section>;
 }
-
-export default Container;
