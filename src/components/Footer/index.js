f11aea109d421511279c81cb376944b22a0c5c19
Ochuko Ekrresa <kelvinekrresa@gmail.com>
2020-03-24 13:34:52 +0100

    Change position of footer to sticky

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import { FaRegCopyright } from 'react-icons/fa';
+import { FaRegCopyright, FaHeart } from 'react-icons/fa';
 
 import './footer.css';
 
 export function Footer() {
 	return (
 		<footer className="footer">
-			<p className="footer-copyright">
-				<FaRegCopyright style={{ marginRight: '3px' }} /> {new Date().getFullYear()}{' '}
+			<p>
+				Created with <FaHeart color="crimson" style={{ margin: '0em .4em' }} /> by
 				<a href="https://github.com/chuck-huey"> Ochuko Ekrresa</a>
+			</p>
+			<p className="footer-copyright">
+				<FaRegCopyright style={{ marginRight: '3px' }} /> Star Wars,{' '}
+				{new Date().getFullYear()}{' '}
 			</p>
 		</footer>
 	);
