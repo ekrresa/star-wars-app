60dd0223aa1308800ab9598a7ed0da02c58bad67
Ochuko Ekrresa <kelvinekrresa@gmail.com>
2020-04-22 18:12:51 +0100

    Edited prop to link up individual pages

--- src/pages/Home/components/ViewButton.js
+++ src/pages/Home/components/ViewButton.js
@@ -6,7 +6,7 @@ import './viewbutton.css';
 export function ViewMoreButton({ name }) {
 	return (
 		<div className="viewbtn-container">
-			<Link to={`/resource/${name}`} className="btn viewbtn">
+			<Link to={`/${name}`} className="btn viewbtn">
 				view more
 			</Link>
 		</div>
