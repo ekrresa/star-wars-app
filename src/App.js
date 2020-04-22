6aa774b7cbebfff24d5cad9900fe99b14721eb35
Ochuko Ekrresa <kelvinekrresa@gmail.com>
2020-04-22 15:04:00 +0100

    Installed react-query-devtools

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import { ReactQueryDevtools } from 'react-query-devtools';
 import Routes from './routes';
 
 function App() {
-	return <Routes />;
+	return (
+		<>
+			<Routes />
+			<ReactQueryDevtools initialIsOpen={false} />
+		</>
+	);
 }
 
 export default App;
