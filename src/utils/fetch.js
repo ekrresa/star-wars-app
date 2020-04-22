c48df309e3e5a68c7ce954554734f78d68ae1a78
Ochuko Ekrresa <kelvinekrresa@gmail.com>
2020-04-22 15:04:40 +0100

    Switch fetch library to react-query

--- src/utils/fetch.js
+++ src/utils/fetch.js
@@ -1,26 +1,42 @@
-import Axios from 'axios';
-import useSWR from 'swr';
+import axios from 'axios';
+import { useQuery } from 'react-query';
 
-export async function fetchData(url, token) {
-	try {
-		const result = await Axios({ url, cancelToken: token });
-		return result.data.results;
-	} catch (error) {
-		console.error(error);
-		return error;
-	}
+export async function fetchMovie(key, resource) {
+	const { REACT_APP_BASE_URL, REACT_APP_MOVIE_TOKEN } = process.env;
+
+	const { data } = await axios({
+		method: 'GET',
+		url: `${REACT_APP_BASE_URL}${resource}`,
+		headers: {
+			Authorization: `Bearer ${REACT_APP_MOVIE_TOKEN}`,
+		},
+	});
+
+	return data;
 }
 
-export function useCancellableSWR(key, swrOptions) {
-	const source = Axios.CancelToken.source();
-	const token = source.token;
+async function fetchResource(key, resource) {
+	const { REACT_APP_STAR_WARS_URL } = process.env;
+	const source = axios.CancelToken.source();
 
-	return [
-		useSWR(key, (url) => fetchData(url, token), {
-			revalidateOnFocus: false,
-			refreshInterval: 0,
-			...swrOptions,
-		}),
-		source,
-	];
+	const { data } = await axios({
+		method: 'GET',
+		url: `${REACT_APP_STAR_WARS_URL}${resource}`,
+		cancelToken: source.token,
+	});
+
+	data.results.cancel = () => {
+		source.cancel('Request was cancelled');
+	};
+
+	return data.results;
+}
+
+export function useCancellableQuery(query) {
+	const { data, error, status } = useQuery(query, fetchResource, {
+		staleTime: Infinity,
+		refetchOnWindowFocus: false,
+	});
+
+	return { data, error, status };
 }
