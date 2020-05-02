b8e4e6b2c9cf7019132b690c9dab9183d15ac9e2
Ochuko Ekrresa <kelvinekrresa@gmail.com>
2020-05-02 18:05:51 +0100

    Added more images

--- src/utils/images.js
+++ src/utils/images.js
@@ -1,9 +1,18 @@
-import starship1 from '../assets/images/starship-1.jpg';
-import starship2 from '../assets/images/starship-2.jpg';
-import starship3 from '../assets/images/starship-3.jpg';
-import starship4 from '../assets/images/starship-4.jpg';
-import starship5 from '../assets/images/starship-5.jpg';
-import starship6 from '../assets/images/starship-6.jpg';
+import millenium from '../assets/images/millenium.jpg';
+import death from '../assets/images/death-star.jpg';
+import imperial from '../assets/images/imperial.webp';
+import corvette from '../assets/images/corvette.jpg';
+import rebel from '../assets/images/rebel-transpo.webp';
+import executor from '../assets/images/executor.jpg';
+import yWing from '../assets/images/y-wing.webp';
+import tieAdvanced from '../assets/images/tie-advanced.jpg';
+import xWing from '../assets/images/x-wing.jpg';
+import sentinel from '../assets/images/sentinel.webp';
+
+import starship1 from '../assets/images/starship-2.jpg';
+import starship2 from '../assets/images/starship-3.jpg';
+import starship3 from '../assets/images/starship-4.jpg';
+import starship4 from '../assets/images/starship-5.jpg';
 
 import planet1 from '../assets/images/planet-1.jpg';
 import planet2 from '../assets/images/planet-2.jpg';
@@ -20,16 +29,29 @@ import anakin from '../assets/images/Anakin-Skywalker.jpeg';
 import beru from '../assets/images/beru-whitesunlars.jpg';
 import solo from '../assets/images/han-solo.jpg';
 import quigon from '../assets/images/qui-gon-jinn.jpg';
+import chewbacca from '../assets/images/chewbacca.jpg';
+import palpatine from '../assets/images/palpatine.jpg';
+import yoda from '../assets/images/yoda.jpg';
+import windu from '../assets/images/mace-windu.jpg';
+import boba from '../assets/images/boba-fett.jpg';
+import padme from '../assets/images/padme.jpg';
+import maul from '../assets/images/darth-maul.jpg';
 import unavailable from '../assets/images/character-unavailable.jpg';
 
-export const starshipImages = [
-	starship4,
-	starship2,
-	starship5,
-	starship1,
-	starship3,
-	starship6,
-];
+export const starshipImages = {
+	'death star': death,
+	'millennium falcon': millenium,
+	'star destroyer': imperial,
+	'cr90 corvette': corvette,
+	'rebel transport': rebel,
+	'y-wing': yWing,
+	'x-wing': xWing,
+	'tie advanced x1': tieAdvanced,
+	'sentinel-class landing craft': sentinel,
+	executor,
+};
+
+export const unknownStarships = [starship1, starship2, starship3, starship4];
 
 export const starPlanets = [planet1, planet2, planet3];
 
@@ -45,5 +67,12 @@ export const starCharacters = {
 	'beru whitesun lars': beru,
 	'han solo': solo,
 	'qui-gon jinn': quigon,
+	chewbacca,
+	palpatine,
+	yoda,
+	'boba fett': boba,
+	'mace windu': windu,
+	'padmé amidala': padme,
+	'darth maul': maul,
 	unavailable,
 };
